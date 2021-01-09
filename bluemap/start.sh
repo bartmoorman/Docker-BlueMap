5f75d789128c6dfef57169c9fcf9db37e27850da
Bart Moorman <github@id10t.us>
2021-01-09 00:00:52 -0700

    Generate initial configs if they don't already exist

--- bluemap/start.sh
+++ bluemap/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -f core.conf -o ! -f render.conf -o ! -f webserver.conf ]; then
+    $(which java) -jar /opt/bluemap/BlueMap-*-cli.jar
+fi
+
 exec $(which java) \
     -jar /opt/bluemap/BlueMap-*-cli.jar \
     --webserver
