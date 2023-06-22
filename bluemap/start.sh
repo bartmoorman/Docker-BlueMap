1e3abdd723d6e92c9d8210775852f9166f9f8d7d
Bart Moorman <github@id10t.us>
2023-06-21 21:59:46 -0600

    Force bluemap to update webapp on start

--- bluemap/start.sh
+++ bluemap/start.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+if [ -f /var/lib/bluemap/web/index.html ]; then
+    rm --verbose --force /var/lib/bluemap/web/index.html
+fi
+
 exec $(which bluemapctl) \
     --verbose \
     --webserver \
